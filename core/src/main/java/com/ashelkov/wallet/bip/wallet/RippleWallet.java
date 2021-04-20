18cacc9391464238827b843aa9056552038f345f
Alex Shelkovnykov <a@ashelkov.com>
2021-04-19 18:27:06 -0700

    Modify Ripple wallet to use curve secp256k1 instead of ed25519

--- core/src/main/java/com/ashelkov/wallet/bip/wallet/RippleWallet.java
+++ core/src/main/java/com/ashelkov/wallet/bip/wallet/RippleWallet.java
@@ -1,6 +1,7 @@
 package com.ashelkov.wallet.bip.wallet;
 
 import org.stellar.sdk.KeyPair;
+import org.web3j.crypto.Bip32ECKeyPair;
 import org.web3j.crypto.Hash;
 
 import com.ashelkov.wallet.bip.util.EncodingUtils;
@@ -17,12 +18,14 @@ public class RippleWallet extends Wallet {
     private static final byte MASTER_PUB_KEY_PREFIX = (byte)0xED;
     private static final byte PAYLOAD_PREFIX = (byte)0x00;
 
+    private final Bip32ECKeyPair masterKeyPair;
     private final byte[] seed;
 
     public RippleWallet(byte[] seed) {
 
         super(Coin.XRP);
 
+        this.masterKeyPair = Bip32ECKeyPair.generateKeyPair(seed);
         this.seed = seed;
     }
 
@@ -60,6 +63,37 @@ public class RippleWallet extends Wallet {
         int[] derivedKeyPath = getDerivedKeyPath(account);
         String bip44Path = Bip44Utils.convertPathToText(derivedKeyPath);
 
+        Bip32ECKeyPair derivedKeyPair = Bip32ECKeyPair.deriveKeyPair(masterKeyPair, derivedKeyPath);
+
+        byte[] accountID = Hash.sha256hash160(derivedKeyPair.getPublicKeyPoint().getEncoded(true));
+        byte[] payload = new byte[21];
+        System.arraycopy(accountID, 0, payload, 1, 20);
+        payload[0] = PAYLOAD_PREFIX;
+
+        byte[] checksum = Hash.sha256(Hash.sha256(payload));
+        byte[] rawAddress = new byte[25];
+        System.arraycopy(payload, 0, rawAddress, 0, 21);
+        System.arraycopy(checksum, 0, rawAddress, 21, 4);
+        String address = EncodingUtils.base58Ripple(rawAddress);
+
+        return new RippleAddress(address, bip44Path);
+    }
+
+    private int[] getDerivedKeyPath(int account) {
+
+        int purpose = PURPOSE_44 | HARDENED;
+        int coinCode = coin.getCode() | HARDENED;
+        int change = 0;
+        int addressIndex = 0;
+
+        return new int[] {purpose, coinCode, account | HARDENED, change, addressIndex};
+    }
+
+    private Bip44Address getAddressED25519(int account) {
+
+        int[] derivedKeyPath = getDerivedKeyPath(account);
+        String bip44Path = Bip44Utils.convertPathToText(derivedKeyPath);
+
         byte[] rawPublicKey = KeyPair
                 .fromSecretSeed(Bip44Utils.deriveEd25519PrivateKey(seed, derivedKeyPath))
                 .getPublicKey();
@@ -81,7 +115,7 @@ public class RippleWallet extends Wallet {
         return new RippleAddress(address, bip44Path);
     }
 
-    private int[] getDerivedKeyPath(int account) {
+    private int[] getDerivedKeyPathED25519(int account) {
 
         int purpose = PURPOSE_44 | HARDENED;
         int coinCode = coin.getCode() | HARDENED;
